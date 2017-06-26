8f54fe53a06213319718f078a4011c55d0225527
twhipple <mrtjwhipple@gmail.com>
2017-06-25 22:09:27 -0400

    Hours and hours of trying I finally got the random number to add to the card.

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,8 +7,20 @@ $(document).ready(function() {
     });
     $('#cardC').click(function()  {
     $(this).css('background-color', 'hsl(90, 85%, 65%');
-    var message = Math.floor((Math.random() * 8) + 1);
-    var $heading = $('<h2>' + message + '</h2>');
+//    $(this).css('color', 'white');
+//    function myFunction(){
+//    var x = document.getElementById('#cardC')
+//    x.html = Math.floor((Math.random() * 8) + 1);
+//    }
+//    console.log(x.innerHTML);
+     var randomNum = Math.floor((Math.random() * 8) + 1);
+     $('#cardC').html(randomNum);
+     console.log(randomNum);
+
+
+//     $('#cardC').append('<h2> + 'randomNum' + </h2>');
+
+    //var $randomCard = $('<h2>' + randomNum + '</h2>');
     });
     $('.cardD').click(function()  {
     $(this).css('background-color', 'hsl(160, 85%, 65%');
